085164b44783c7f86ddba57b26c3d76509548ba5
Bob <bob.lund@gmail.com>
2026-07-08 20:25:58 -0600

    net -> socket

--- http.mjs
+++ http.mjs
@@ -1,9 +1,9 @@
-import * as net from 'net.mjs';
-import { stringToUint8 } from 'abConversions.mjs';
+import * as os from 'os';
+import * as std from 'std';
+import { Server } from 'socket.so';
+import { TextEncoder } from './EncodeDecode.mjs';
 
-function aBufToString(buffer) {
-	return String.fromCharCode.apply(null, new Uint16Array(buffer));
-}
+const enc = new TextEncoder;
 
 function parseRequest( data ) {
 	const str = data.toString();
@@ -18,18 +18,23 @@ function parseRequest( data ) {
 	return { method, path, protocol, headers };
 }
 
+function writeStr( fd, str ){
+	const buf = enc.encode( str );
+	os.write( fd, buf.buffer, 0, buf.length );
+}
+
 class Response{
-	#socket;
+	#fd;
 	#headers = [];
 	#chunked = false;
 
-	constructor( socket ){ this.#socket = socket; }
+	constructor( fd ){ this.#fd = fd; }
 	end( aBuf = undefined ){
-		if( aBuf ){ this.write( aBuf ); }
+		if( aBuf ){ os.write( this.#fd, aBuf, 0, aBuf.byteLength ); }
 		if( this.#chunked ){
-			this.#socket.write( stringToUint8( '0\r\n\r\n' ).buffer );
+			writeStr( this.#fd, '0\r\n\r\n' );
 		}else{
-			//this.#socket.end();
+			//close this.#fd?
 		}
 	}
 
@@ -46,57 +51,93 @@ class Response{
 	write( aBuf ){
 		if( this.#headers.length > 0 ){
 			this.#headers.unshift( `HTTP/1.1 ${ this.statusCode } ${ this.statusMessage }` );
-			this.#socket.write( stringToUint8( this.#headers.join( '\r\n' ) + '\r\n\r\n' ).buffer );
+			writeStr( this.#fd, this.#headers.join( '\r\n' ) + '\r\n\r\n' );
 			this.#headers = [];
 		}
 
 		if( this.#chunked ){
-			this.#socket.write( stringToUint8( `${ aBuf.byteLength.toString( 16 ) }\r\n` ).buffer );
-			this.#socket.write( aBuf );
-			this.#socket.write( stringToUint8( '\r\n' ).buffer );
+			writeStr( this.#fd, `${ aBuf.byteLength.toString( 16 ) }\r\n` );
+			os.write( this.#fd, aBuf, 0, aBuf.byteLength );
+			writeStr( this.#fd, '\r\n' );
 		}else{
-			this.#socket.write( aBuf );
+			os.write( this.#fd, aBuf, 0, aBuf.byteLength );
 		}
 	}
 }
 
-export function createServer( func ){
+export function createServer( httpServerCb ){
+	const server = new Server();
 	let wsUpgradeCb = undefined;
-	let _socket;
-	let netSocket = net.createServer( ( socket ) => {
-		_socket = socket;
-		socket.on( 'data', readBuf => {
-			const req = parseRequest( String.fromCharCode( ...new Uint8Array( readBuf.buffer, 0, readBuf.buffer.length ) ) );
-			if( req.protocol != "HTTP/1.1" ){
-				console.log( 'Unknown request protocol' );
-				socket.end();
-				return;
+
+	function listen( { port, key, cert } ){
+		const READBUF_CHUNK_SIZE = 4096;
+		const fdBuff = new Int32Array( 2 ); //( https ? 2 : 1 );
+		const { stop, pipe_fd } = server.listen( { port, key, cert } );
+
+		os.setReadHandler( pipe_fd, () => {
+			if( os.read( pipe_fd, fdBuff.buffer, 0, fdBuff.length * 4 ) > 0 ){
+				const fds = Array.from( fdBuff );
+				/*const socket = new class{
+					read_fd = fds[ 0 ];
+					write_fd = fds[ 1 ];
+					listeners = {
+						data: () => {},
+						close: () => {},
+						error: () => {}
+					};
+					end(){ server.end( socket.read_fd ); }
+					on( event, func ){
+						this.listeners[ event ] = func;
+						return func;
+					};
+					write( aBuf ){ os.write( this.write_fd, aBuf, 0, aBuf.byteLength ); }
+				};*/
+
+				os.setReadHandler( fds[ 0 ], () => {
+					let readBuf = new Uint8Array( READBUF_CHUNK_SIZE );
+					let n;
+					if ( ( n = os.read( fds[ 0 ], readBuf.buffer, 0, readBuf.length ) )  > 0 ) {
+						readBuf = readBuf.slice( 0, n );
+						const req = parseRequest( String.fromCharCode( ...new Uint8Array( readBuf.buffer, 0, readBuf.buffer.length ) ) );
+						if( req.protocol != "HTTP/1.1" ){
+							console.log( 'Unknown request protocol:', req.protocol, 'on fd:', fds[ 0 ] );
+							//socket.end();
+							server.end( fds[ 0 ] );
+							return;
+						}
+						if( req.headers?.[ "upgrade" ] == "websocket" ){
+							console.log( `http server websocket upgrade` );
+							os.setReadHandler( fds[ 0 ], null );
+							if( wsUpgradeCb ){
+								wsUpgradeCb( req.headers, [ ...fds ] );
+							}else{
+								console.log( `no websocket upgrade callback` );
+							}
+							return;
+						}
+						httpServerCb( req, new Response( fds[ 1 ] ) );
+						readBuf.fill( 0 );
+						return;
+					}
+					if( n === 0 ){
+						console.log( `client disconnected` );
+					} else {
+						console.log( n == 54 ? `client (fd ${ fds[ 0 ] }) closed` : `server error fds[ 0 ]: ${ fds[ 0 ] }, n: ${ n }` );
+						std.exit( 1 );
+					}
+					os.setReadHandler( fds[ 0 ], null );
+					os.close( fds[ 0 ] ); os.close( fds[ 1 ] );
+					fds[ 0 ] = fds[ 1 ] = -1;
+				} );
+			} else {
+				os.close( pipe_fd );
+				stop();
 			}
-			if( req.headers?.[ "upgrade" ] == "websocket" ){
-				console.log( `http server websocket upgrade` );
-				if( wsUpgradeCb ){
-					wsUpgradeCb( req.headers, socket );
-				}else{
-					console.log( `no websocket upgrade callback` );
-				}
-				return;
-			}
-			func( req, new Response( socket ) );
 		} );
+	}
 
-		socket.on( 'close', () => {
-			console.log( `client disconnected` );
-		} );
-
-		socket.on( 'error', n => {
-			console.log( n == 54 ? `client (fd ${ socket.fd }) closed` : `server error: ${ n }` );
-		} );
-
-		return socket;
-	} );
-
-	return new class{
-		listen( { port, key, cert } ){ netSocket.listen( { port, key, cert } ); }
+	return {
+		listen,
 		wsUpgrade( func ){ wsUpgradeCb = func; }
 	};
 }
